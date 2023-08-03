35042c847a0fa2d481dba3026c1593a5a93bc5da
Maomao <tim880911@gmail.com>
2023-08-03 12:34:51 +0800

    complete

--- list.py
+++ list.py
@@ -1,11 +1,14 @@
+import os # operating system
+
 #讀取檔案
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #繼續
-		n, p = line.strip().split(',')
-		products.append([n, p])
+if os.path.isfile('products.csv'):
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續
+			n, p = line.strip().split(',')
+			products.append([n, p])
 
 #使用者輸入
 while True:
@@ -18,7 +21,7 @@ while True:
 #印出所有紀錄
 for p in products:
 	print(p[0], '的價格是', p[1])
-	
+
 #寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
