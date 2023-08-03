ab0eaae0e9fb09e38c14f523869a925614e7af0b
Maomao <tim880911@gmail.com>
2023-08-03 12:12:18 +0800

    add read file

--- list.py
+++ list.py
@@ -1,6 +1,13 @@
-
-
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue #繼續
+		n, p = line.strip().split(',')
+		products.append([n, p])
+
+#使用者輸入
 while True:
 	n = input('輸入名稱: ')
 	if n == 'q':
@@ -8,6 +15,11 @@ while True:
 	p = int(input('請輸入價格: '))
 	products.append([n, p])
 
+#印出所有紀錄
+for p in products:
+	print(p[0], '的價格是', p[1])
+	
+#寫入檔案
 with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
